c9149f989a652757c6b68789773ee1da4f582a5d
StasTikhomirov <stastih97@gmail.com>
2017-11-06 23:48:48 +0300

    Создал главную страницу.

---
+++ Main.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 4.01 Transitional//EN" 
+"http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
+
+<title>Интернет-магазин Велотоваров</title>
+</head>
+
+<body>
+<style>
+body{
+	background-image:url(background.jpg);
+	font-family:Cambria;
+	color:rgba(206,0,3,1.00);
+}
+
+
+A {
+    text-decoration: none; /* Убирает подчеркивание для ссылок */
+} 
+
+A:hover { 
+    text-decoration: underline; /* Добавляем подчеркивание при наведении курсора на ссылку */
+    color: red; /* Ссылка красного цвета */
+} 
+
+p { text-indent: 200px; }
+
+input.text {
+    
+    height: 25px;
+	font-size:18px;
+    
+}
+
+}
+</style>
+
+<table  border="0" align="right" width="100%">
+<tr><img src="ikon.jpg" align="left" width="176"></td>
+<font align="right" size="25" >Интернет-магазин Велотоваров</font></td>
+</tr>
+
+<tr>
+<i><b><p align="left"><font align="center" size="25" >BikeGoods</font></p></b></i>
+</tr>
+<tr></tr><tr></tr>
+
+<tr>
+<td></td>
+<td bgcolor="#016C14" align="center" width="150"><font  size="6"><a href="Autorisation.html">Вход</a></td>
+<td bgcolor="#016C14" align="center" width="150"><font size="6" ><a href="Registration.html">Регестрация</a></td>
+<td></td>
+<td></td>
+</tr>
+
+</table>
+
+
+<table border="0" align="right" width="100%">
+<tr height="20"></tr><tr><td width="30"></td><td >
+<input align="left" id="quer" type=text class=text name="query" value="" placeholder="Поиск по каталогу" size="60" height="50"></td></tr>
+</table>
+
+<table border="0" align="right" width="100%">
+<tr>
+<td align="center" width="200"><font size="15"><a href="Catalog.html">Каталог</a></td>
+<td align="center" width="200"><font size="15"><a href="Orders.html">Заказы</a></td>
+<td align="center" width="200"><font size="15"><a href="Contacts.html">Контакты</a></td>
+<td></td>
+</tr>
+
+
+
+
+</table>
+
+
+
+
+
+  
+
+</body>
+</form>
+</html>
+
