7be3933574ac547afc48d6b2023acb2860463bef
StasTikhomirov <stastih97@gmail.com>
2017-11-17 20:32:25 +0300

    Нормальное меню + вид главной страницы

--- Main.php
+++ Main.php
@@ -5,27 +5,50 @@
 <meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
 
 <title>VeloGoods</title>
-<style type="text/css" media="all">@import "Main.css";</style>
+<link rel="stylesheet" type="text/css" href="Main.css">
 </head>
 
 <body>
 
 <header>
-<div><img class="logo" src="logo.png" ></div>
+<div><img id="Logo" src="Shop_logo.png" ></div>
 
-<div class="Authorisation"><a href="Autorisation.html">Вход</a></div>
-<div class="slash"><p>/</p></div>
-<div class="Sign_up"><a href="Registration.html">Регистрация</a></div>
+<div id="Sign">
+<a href="Authorisation.php">Вход</a>
+<span>/</span>
+<a href="regis.html">Регистрация</a>
+</div>
+
 <div class="Basket"><a href="Basket.html">Корзина</a></div>
+
+
 </header>
 
-<section id="menu">
-<div class="catalog"><a href="Catalog.html">Каталог</a></div>
-<div class="contacts"><a href="Contacts.html">Контакты</a></div>
-</font>
+
+	
 
 <input id="search_area" type=text name="query" class=text value="" placeholder="  Поиск по каталогу" size="60" height="50"></div>
 
+
+<ul class="menu">
+    <li><a href=#>Главная</a>       
+    </li>
+    <li><a href=#>Каталог</a>
+        <ul class="submenu">
+            <li><a href=#>Запчати</a></li>
+            <li><a href=#>Одежда</a></li>
+            <li><a href=#>Аксессуары</a></li>
+        </ul>
+    </li>
+    <li><a href=#>Новости</a>
+        <ul class="submenu">
+            <li><a href=#>Новые товары</a></li>
+            <li><a href=#>Акции</a></li>
+        </ul>
+    </li>
+    <li><a href=#>Контакты</a>        
+    </li>
+</ul>
 
 
 
---
+++ Sign.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Вход/Регистрация</title>
+<style type="text/css" media="all">@import "Sign.css";</style>
+</head>
+
+
+<body>
+
+<ul class="menu">
+    <li><a href=#>Menu 1</a>
+        <ul class="submenu">
+            <li><a href=#>Sudmenu 1</a></li>
+            <li><a href=#>Sudmenu 1</a></li>
+            <li><a href=#>Sudmenu 1</a></li>
+        </ul>
+    </li>
+    <li><a href=#>Menu 2</a>
+        <ul class="submenu">
+            <li><a href=#>Sudmenu 2</a></li>
+            <li><a href=#>Sudmenu 2</a></li>
+            <li><a href=#>Sudmenu 2</a></li>
+        </ul>
+    </li>
+    <li><a href=#>Menu 3</a>
+        <ul class="submenu">
+            <li><a href=#>Sudmenu 3</a></li>
+            <li><a href=#>Sudmenu 3</a></li>
+            <li><a href=#>Sudmenu 3</a></li>
+        </ul>
+    </li>
+    <li><a href=#>Menu 4</a>
+        <ul class="submenu">
+            <li><a href=#>Sudmenu 4</a></li>
+            <li><a href=#>Sudmenu 4</a></li>
+            <li><a href=#>Sudmenu 4</a></li>
+        </ul>
+    </li>
+    <li><a href=#>Menu 5</a>
+        <ul class="submenu">
+            <li><a href=#>Sudmenu 5</a></li>
+            <li><a href=#>Sudmenu 5</a></li>
+            <li><a href=#>Sudmenu 5</a></li>
+        </ul>
+    </li>
+</ul>
+</body>
+</html>
