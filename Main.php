cbec124b917dd18b3d23f33876227306cc99474e
StasTikhomirov <stastih97@gmail.com>
2017-12-12 13:52:37 +0300

    Главная страница

--- Main.php
+++ Main.php
@@ -11,29 +11,12 @@
 <body>
 
 <header>
-<div><img id="Logo" src="Shop_logo.png" ></div>
-
-<div id="Sign">
-<a href="Authorisation.php">Вход</a>
-<span>/</span>
-<a href="regis.html">Регистрация</a>
-</div>
-
-<div class="Basket"><a href="Basket.html">Корзина</a></div>
-
-
-</header>
-
-
-	
-
-<input id="search_area" type=text name="query" class=text value="" placeholder="  Поиск по каталогу" size="60" height="50"></div>
-
+<div><img id="Logo" src="images/Shop_logo.png" ></div>
 
 <ul class="menu">
     <li><a href=#>Главная</a>       
     </li>
-    <li><a href=#>Каталог</a>
+    <li><a href="Catalog.php">Каталог</a>
         <ul class="submenu">
             <li><a href=#>Запчати</a></li>
             <li><a href=#>Одежда</a></li>
@@ -51,20 +34,87 @@
 </ul>
 
 
-<a href="#ModalOpen" title="">Открыть окно</a>
-<div id="ModalOpen" class="Window">
- <div>
-  <a href="#close" title="Закрыть" class="close">X</a>
-  <h2>Модальное окно</h2>
-
-        <p>Пример простого модального окна, которое может быть создано с использованием CSS3.</p>
-
-        <p>Его можно использовать в широком диапазоне, начиная от вывода сообщений и заканчивая формой регистрации.</p>
-
- </div>
+<div id="Sign">
+	<form method="post">
+    <table>    
+    <td>Логин</td>
+		<td><input type="text" class="subm" name="login" value=""         > </td>
+		</tr>
+        
+        <tr>		
+		<td>Пароль</td>
+		<td><input type="text" class="subm" name="password" value=""    > </td>
+       </tr>
+      
+      </table>
+      
+    <input  type="submit" class="subm" name="Log" value="Войти">	
+    <span>/</span>
+    
+    <a href="Autorisation.php" id="registrate">Регистрация</a>
+    </form>
 </div>
 
 
+
+</header>
+
+
+
+<?
+if (isset($_POST['Log']))
+{
+	$link = mysqli_connect("localhost", "root", "", "bikegoods"); // адрес хоста, пользователь, пароль, название БД
+		
+				
+		$login=$_POST['login'];
+		$password=$_POST['password'];
+		
+		$query = "SELECT Firstname FROM `users` WHERE Login='$login' AND password='$password' ";
+				$result = mysqli_query($link, $query); // result = ответ БД на запрос квери по соединению линк
+			
+			$res=mysqli_fetch_array($result);
+			
+		if ($res['Firstname'] == true) {
+			echo "Данные успешно Найдены!";
+			echo $res['0'];
+		}
+		else {
+			echo "Произошла ошибка, пожалуйста повторите попытку.";
+		} 
+
+}
+?>
+
+<div id="ModalOpen" class="Window">
+     
+     <div>
+      <a href="#close" title="Закрыть" class="close">X</a>
+            
+      <table align="center">
+      <tr><td></td><td>Вход в систему</td><td></td></tr>
+      <tr style="height:10px"><tr>
+      <tr>		
+		<td>Логин</td>
+		<td><input type="text" class="subm" name="login" value=""         > </td>
+	</tr>
+        
+       <tr>		
+		<td>Пароль</td>
+		<td><input type="text" class="subm" name="password" value=""    > </td>
+	</tr>        
+        <tr><td style="height:10px"></td></tr>
+       </table>
+       
+       
+       
+       <div align="center"> <input  type=submit class=subm name=Logsa value="Войти">	</div>
+      
+     </div> 
+    </div>
+	
+
+<input id="search_area" type=text name="query" class=text value="" placeholder="  Поиск по каталогу" size="60" height="50"></div>
 
 </body>
 </form>
