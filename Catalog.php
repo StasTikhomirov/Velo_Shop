ff264447d9ec1f020b8906edd961ead13bab12a4
StasTikhomirov <stastih97@gmail.com>
2017-12-17 16:15:41 +0300

    Оформил почти до конца, осталась передача данных другим страницам

--- Catalog.php
+++ Catalog.php
@@ -1,4 +1,5 @@
-﻿<!DOCTYPE html>
+﻿
+<!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
@@ -31,10 +32,15 @@
 </ul>
 
 </header>
+<div id="welc"><h1 >Dmitry Bessonov</div>
 
+<form action="" method="post">
     <div id="container">
        
         	<?					
+			
+			
+				
 				$link = mysqli_connect("localhost", "root", "", "bikegoods"); // адрес хоста, пользователь, пароль, название БД
 
 				if (!$link) { // если нет подключения
@@ -43,48 +49,74 @@
 					echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
 					exit;
 				}
-				$query = "SELECT * FROM `products` ";
+				$query = "SELECT * FROM `products`";
 				$result = mysqli_query($link, $query); // result = ответ БД на запрос квери по соединению линк
 			?>
-			<table class="table_col">
-                <colgroup>
-                    <col style="background:rgba(0,0,0,1.00);">
-                </colgroup>
+            <h2 align="center">Наши товары</h2>
+			<table id="table" width="1200" border="1px">
+                
                 <tr>
                     <th>Название</th>
                     <th>Описание</th>
-                    <th>Цена</th>
-                    <th>Код Разновидности</th>  
-                                  
+                    <th>Цена</th> 
+                    <th>Заказать</th>                                                      
                 </tr>
 				<?php
-					$i=0;
+					$Product;
+					$i=1;
+					$id="Name";
 						
 						while($eachProduct = mysqli_fetch_assoc($result)){ //на каждой итерации берем 1 из результатов (строк из БД) и обрабатываем.
 						$name = $eachProduct['Name'];
 						$description = $eachProduct['Description'];
-					    $cost = $eachProduct['Price'];
-						$typeId = $eachProduct['ProductTypeID'];										
-											
+					    $cost = $eachProduct['Price'];											
+						$Product="Tovar".$i;
+						$Prodname=$id.$i;
+						
+						 //setcookie("Product[i]",$name);
+						
 						echo "<tr>
-								<td>$name</td>
-								<td>$description</td>
-								<td>$cost</td>
-								<td align=\"center\">$typeId</td>
-								
-								
+								<td width=150 align=\"center\">$name<input type=\"radio\" name='$Prodname' value='$name' checked hidden='true'></td>
+								<td width=1000 align=\"center\">$description</td>
+								<td width=75 align=\"center\">$cost</td>
+								<td align=\"center\"><input type=\"checkbox\" name='$Product'></td>					
 							</tr>";
-							
+						$i++;
 					} // while
 				?>
                 
-            
-            
-        </div>
-        
-            <div id="order" align="center">	
-            <input type="submit" class="submit" name="order"  value="Заказать">	
-            </div>
-    
+        </table>
+    </div>
+    <div id="order" align="center">	
+            <input type="submit" class="submit" name="order"  value="Заказать">	</div>
+ </form>   
+    		
+            <form action="order.php" method="post"><a href="orders.php">Order</a></form>           
+
+    <?
+				$y=1;
+			
+				if (isset($_POST['order']))
+				{
+					while($y<=$i)
+					{
+						$tovar="Tovar".$y;
+						$tovName="Name".$y;
+						$tovCost="Cost".$y;
+						
+						if (isset($_POST[$tovar]))
+						{	
+							$orderName =($_POST[$tovName])	;
+							$orderCost =($_POST[$tovCost])	;
+							echo $order;	
+										   
+						}
+						
+						$y++;
+					}
+						
+				}
+			?>
 </body>
 </html>
+
